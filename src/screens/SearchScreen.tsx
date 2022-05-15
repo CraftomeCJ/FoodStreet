53e2d785c89a68e464de9ebc8b00a67a3ad5737d
javier ho <tech@craftome.com>
2022-05-15 19:46:55 +0800

    Extracting Hook

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -3,37 +3,16 @@ import {
   Text,
   StyleSheet
 } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import SearchBar from '../components/SearchBar'
-import yelp from '../api/yelp';
+import useResults from "../hooks/useResults"
 
 const SearchScreen: React.FC = () => {
 
     const [term, setTerm] = useState<string>('');
-    const [results, setResults] = useState<any[]>([]);
-    const [errorMessage, setErrorMessage] = useState<string>('');
+    const [searchApi, results, errorMessage] = useResults();
 
-      const searchApi = async (searchTerm: string) => {
-
-        try {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 30,
-        term: searchTerm,
-        location: 'singapore'
-      }
-    });
-
-    setResults(response.data.businesses);
-        } catch (e) {
-          setErrorMessage('Oops!!Something went wrong');
-        }
-  };
-
-  useEffect(() => {
-    searchApi('paste');
-  }, []);
 
     return (
       <View>
